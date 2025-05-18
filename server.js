cb092c9b4060d1a89731d93d60d3a8146ed1c9f8
ktihomirov96 <ktihomirov@abv.bg>
2025-05-18 16:20:56 +0300

    Add files via upload

--- server.js
+++ server.js
@@ -1,6 +1,13 @@
-
 const express = require('express');
 const app = express();
+const path = require('path');
+
 app.use(express.static(__dirname));
-app.get('/', (req, res) => res.sendFile(__dirname + '/index.html'));
-app.listen(3000, () => console.log('Сървърът е стартиран на http://localhost:3000'));
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, 'index.html'));
+});
+
+const port = process.env.PORT || 3000;
+app.listen(port, () => {
+  console.log('Сървърът работи на порт ' + port);
+});
