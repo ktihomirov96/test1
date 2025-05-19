7a2b38f1fd599dcc3f652d7c755883f01f2ce2c8
ktihomirov96 <ktihomirov@abv.bg>
2025-05-19 10:31:05 +0300

    Add files via upload

--- server.js
+++ server.js
@@ -1,7 +1,10 @@
 
-const express = require('express');
-const path = require('path');
+const express = require("express");
+const path = require("path");
 const app = express();
 app.use(express.static(__dirname));
-app.get('/', (req, res) => res.sendFile(path.join(__dirname, 'index.html')));
-app.listen(process.env.PORT || 3000);
+app.get("/", (req, res) => res.sendFile(path.join(__dirname, "index.html")));
+app.get("/offers", (req, res) => res.sendFile(path.join(__dirname, "offers.html")));
+app.get("/requests", (req, res) => res.sendFile(path.join(__dirname, "requests.html")));
+app.get("/login", (req, res) => res.sendFile(path.join(__dirname, "login.html")));
+app.listen(process.env.PORT || 3000, () => console.log("Сървърът е активен."));
