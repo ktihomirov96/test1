f9fbf65423bc37330fbe92a15ffbbe07ebbbc57e
ktihomirov96 <ktihomirov@abv.bg>
2025-05-20 10:44:38 +0300

    Add files via upload

--- server.js
+++ server.js
@@ -3,10 +3,10 @@ const express = require('express');
 const path = require('path');
 const app = express();
 
-app.use(express.static('public'));
+app.use(express.static(__dirname));
 
 app.get('/', (req, res) => {
-  res.sendFile(path.join(__dirname, 'public', 'login.html'));
+  res.sendFile(path.join(__dirname, 'login.html'));
 });
 
 const PORT = process.env.PORT || 3000;
